fa9760d67be8f48d40c39f74426a09ae0f9bb4e0
andrefangeloni <andrefangeloni@gmail.com>
2019-08-02 18:28:17 -0300

    Adicionado Unform

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,20 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Form, Input } from "@rocketseat/unform";
 import logo from "~/assets/logo.svg";
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
 
-      <form>
-        <input placeholder="Digite o nome completo" />
-        <input type="email" placeholder="Digite o e-mail" />
-        <input type="password" placeholder="Digite sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Digite o nome completo" />
+        <Input name="password" type="email" placeholder="Digite o e-mail" />
+        <Input name="password" type="password" placeholder="Digite sua senha" />
 
         <button type="submit">Acessar</button>
         <Link to="/">Já possui cadastro? Acesse agora</Link>
-      </form>
+      </Form>
     </>
   );
 }
